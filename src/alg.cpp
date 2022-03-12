72456b5e2bd6178925ceb7e50c856f6706a5999c
Алексей Смирнов <99120962+AleksGerich@users.noreply.github.com>
2022-03-12 16:20:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,45 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int itog = 0, tmp = 0;
+    for(int i = 0; i < size; i++) {
+        for(int j = (size - 1); j >= (i + 1); j--) {
+            if(arr[j] < arr[j - 1]) {
+                tmp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = tmp;
+            }
+        }
+    }
+    bool flag = false;
+    int l = 0;
+    int r = size--;
+    int mid;
+    while ((l < r) && (flag != true)) {
+        mid = (l + r) / 2;
+        if (arr[mid] < value) {
+            l = mid + 1;
+        }
+        if (arr[mid] > value) {
+            r = mid - 1;
+        }
+        if (arr[mid] == value) {
+            for (int i = mid + 1; i < r; ++i) {
+                if (arr[i] == value) {
+                    itog++;
+                } else {
+                    break;
+                }
+            }
+            for (int i = mid; i > l; --i) {
+                if (arr[i] == value) {
+                    itog++;
+                } else {
+                    break;
+                }
+            }
+            flag = true;
+        }
+    }
+    return itog;
 }
