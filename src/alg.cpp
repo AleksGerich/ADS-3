54e7eb6d5f0e63ba79422404a9c19e37735d81e8
Алексей Смирнов <99120962+AleksGerich@users.noreply.github.com>
2022-03-12 17:04:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,19 +2,21 @@
 
 int cbinsearch(int *arr, int size, int value) {
     int itog = 0, tmp = 0;
-    for(int i = 0; i < size; i++) {
-        for(int j = (size - 1); j >= (i + 1); j--) {
-            if(arr[j] < arr[j - 1]) {
+    for (int i = 0; i < size; i++) {
+        for (int j = (size - 1); j >= (i + 1); j--) {
+            if (arr[j] < arr[j - 1]) {
                 tmp = arr[j];
                 arr[j] = arr[j - 1];
                 arr[j - 1] = tmp;
             }
         }
     }
+
     bool flag = false;
     int l = 0;
     int r = size--;
     int mid;
+
     while ((l < r) && (flag != true)) {
         mid = (l + r) / 2;
         if (arr[mid] < value) {
@@ -24,14 +26,14 @@ int cbinsearch(int *arr, int size, int value) {
             r = mid - 1;
         }
         if (arr[mid] == value) {
-            for (int i = mid + 1; i < r; ++i) {
+            for (int i = mid + 1; i <= r; ++i) {
                 if (arr[i] == value) {
                     itog++;
                 } else {
                     break;
                 }
             }
-            for (int i = mid; i > l; --i) {
+            for (int i = mid; i >= l; --i) {
                 if (arr[i] == value) {
                     itog++;
                 } else {
